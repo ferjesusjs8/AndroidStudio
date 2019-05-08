0ca3fdb2c0c7965b65d704a6143611eda9ed168f
fernando.js8 <fernando.js8@outlook.com>
2019-05-07 22:53:40 -0300

    In Progress

--- ProinterV/app/src/main/java/com/anhanguera/prointerv/MainActivity.java
+++ ProinterV/app/src/main/java/com/anhanguera/prointerv/MainActivity.java
@@ -2,6 +2,8 @@ package com.anhanguera.prointerv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe"};
+        ListView listaAluno = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAluno.setAdapter(adapter);
     }
 }
