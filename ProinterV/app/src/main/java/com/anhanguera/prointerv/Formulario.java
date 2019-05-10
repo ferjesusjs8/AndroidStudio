c09640d96d6d1698b11731bb38d0a48e6d0c9938
fernando.js8 <fernando.js8@outlook.com>
2019-05-10 07:57:52 -0300

    In progress

--- ProinterV/app/src/main/java/com/anhanguera/prointerv/Formulario.java
+++ ProinterV/app/src/main/java/com/anhanguera/prointerv/Formulario.java
@@ -3,6 +3,9 @@ package com.anhanguera.prointerv;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,15 +16,25 @@ public class Formulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+    }
 
-        Button botaoSalvar = findViewById(R.id.formulario_salvar);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
 
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
                 Toast.makeText(Formulario.this, "Salvo com sucesso!", Toast.LENGTH_SHORT).show();
                 finish();
-            }
-        });
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
