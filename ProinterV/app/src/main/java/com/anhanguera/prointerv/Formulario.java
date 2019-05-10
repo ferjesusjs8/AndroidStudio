517c047e0c9224e7523a043765c21f629c9a8457
fernando.js8 <fernando.js8@outlook.com>
2019-05-09 23:20:48 -0300

    in progress

--- ProinterV/app/src/main/java/com/anhanguera/prointerv/Formulario.java
+++ ProinterV/app/src/main/java/com/anhanguera/prointerv/Formulario.java
@@ -2,6 +2,9 @@ package com.anhanguera.prointerv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Formulario extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Formulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Button botaoSalvar = findViewById(R.id.formulario_salvar);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Formulario.this, "Dados salvos com sucesso!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
